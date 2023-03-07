521f3359daee587c17da4ae1297b462dc8acd28b
Enavy <gusdk1793@gmail.com>
2023-03-07 11:10:18 +0900

    System.gc() 메서드 테스트

---
+++ SystemGCTest.java
@@ -0,0 +1,28 @@
+//System.gc() 메서드를 실행했을 때의 시간 측정을 해보자.
+public class SystemGCTest {
+    public static void main(String[] args) {
+        
+        noSystemGc(); // 결과값 : 내 pc기준으로 100ns로 나온다.
+
+        systemGc(); // 결과값 : 9397800ns
+
+
+    }
+
+    static void noSystemGc() {
+        long startTime = System.nanoTime(); //System.nanoTime() : long 타입으로 나노세컨드(1/1000000000) 단위로 반환함 / System.currentTimeMillis() : 밀리세컨드(1/1000초) 단위로 반환함
+        long endTime = System.nanoTime();
+
+        System.out.println(endTime - startTime + "ns"); // 결과값 : 내 pc기준으로 200ns로 나온다.
+    }
+
+    static void systemGc() {
+        long startTime = System.nanoTime(); 
+
+        System.gc();
+
+        long endTime = System.nanoTime();
+
+        System.out.println(endTime - startTime + "ns"); 
+    }
+}
