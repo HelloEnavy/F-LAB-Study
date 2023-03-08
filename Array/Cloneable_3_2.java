45400a906b9077dd3e60e26fc5f4ba6018decf19
Enavy <gusdk1793@gmail.com>
2023-03-08 19:27:13 +0900

    Array Cloneable test

---
+++ Array/Cloneable_3_2.java
@@ -0,0 +1,11 @@
+public class Cloneable_3_2 {
+    public static void main(String[] args) {
+
+        int[] arr1 = new int[5];
+        int[] arr2 = arr1;
+
+        System.out.println(arr1);
+        System.out.println(arr2);
+        
+    }
+}
