4ff77f2c06df4698eb10078782cc3be007c0f16a
HelloEnavy <gusdk1793@gmail.com>
2023-03-11 01:07:22 +0900

    SystemGCTest 파일 이름 수정

--- GarbageCollector/SystemGCTest_3_2.java
+++ GarbageCollector/SystemGCTest_3_2.java
@@ -1,5 +1,5 @@
 //System.gc() 메서드를 실행했을 때의 시간 측정을 해보자.
-public class SystemGCTest {
+public class SystemGCTest_3_2 {
     public static void main(String[] args) {
         
         noSystemGc(); // 결과값 : 내 pc기준으로 100ns로 나온다.
