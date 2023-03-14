caf39c3179d31b7255ecfd43ab7b7d8f59cf74c3
Enavy <gusdk1793@gmail.com>
2023-03-14 20:44:20 +0900

    Iterator에 대한 궁금증이 생겼다..

--- Collection/LinkedHashSetTest.java
+++ Collection/LinkedHashSetTest.java
@@ -50,4 +50,8 @@ public class LinkedHashSetTest {
             System.out.print(it2.next() + " "); // a b c d e f x y z ㄱ ㄴ ㄷ ㄹ -> 추가한 순서대로 출력된다.
         }
     }
+
+    //추가로 확인한 점!
+    //데이터 타입으로 Integer을 넣고 Integer 타입을 출력하였을 때에는 HashSet도 순서대로 나왔다.
+    //Iterator이 알아서 숫자는 정렬시킨 것인가..? 내부 코드를 확인하였을 땐 별다른 코드가 없는뎅..
 }
