63bc921af433be15cc24aba6f06d918189cb969e
Enavy <gusdk1793@gmail.com>
2023-03-14 09:29:17 +0900

    ArrayList remove() 메소드 테스트

---
+++ Collection/RemoveArrayList.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+public class RemoveArrayList {
+    public static void main(String[] args) {
+        
+        ArrayList<String> list = new ArrayList<>();
+
+        System.out.println(list.size());
+
+        for(int i=0 ; i<10 ; i++) { //ArrayList는 처음 제공되는 사이즈를 10으로 주기 때문에 10개의 요소를 넣어봤다.
+            list.add(i, ""+i+"");
+        }
+
+        System.out.println(list.size()); //size = 10
+
+        //요소 하나를 삭제
+        list.remove("3");
+
+        System.out.println(list.size());
+    }
+}
