ecb9c0188aea67754bdf987b7af7e731d9496683
HelloEnavy <gusdk1793@gmail.com>
2023-04-04 21:23:45 +0900

    외부 text 파일을 이용하여 InputStream 구현

---
+++ Stream/InputStreamTest.java
@@ -0,0 +1,37 @@
+package Stream;
+
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.*;
+
+//InputStream 구현
+public class InputStreamTest {
+	
+	public static void main(String[] args) {
+		
+		try {
+			FileInputStream fis = new FileInputStream("C:\\Users\\gusdk\\Downloads\\InputStreamTextFile.txt");
+		
+			int c; //읽어온 데이터가 저장될 변수
+			
+			while((c = fis.read()) != -1) { //읽어올 데이터가 있다면
+				
+				System.out.print((char)c);
+			}
+			
+			fis.close(); //작업이 완료되면 close() 메서드를 이용하여 닫아준다.
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		        
+	}
+
+    //결과값
+    //This is InputStreamTextFile!
+    //text 파일에 적혀있는 문구가 출력되었다.
+}
+
