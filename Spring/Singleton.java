e0c45e0c45897802bfa2312e7412a6371d145810
l-lnaln <gusdk1793@gmail.com>
2023-05-13 20:26:11 +0900

    0513

--- Spring/Singleton.java
+++ Spring/Singleton.java
@@ -1,6 +1,6 @@
 package Spring;
 
-//inner class를 사용하여 singleton 구현
+//inner class를 사용하여 singleton 구현.
 public class Singleton {
 
     private Singleton(){}
@@ -12,6 +12,6 @@ public class Singleton {
     public static Singleton getInstance() {
         return SingletonHelper.INSTANCE;
     }
-    
+
     //inner class 같은 경우는 클래스가 로드될 때 객체가 생성되기 때문에 멀티스레드 환경에서 안전하다
 }
