1605a9bd3a22aa84078a52f15a02d7834759e656
erinleeme <gusdk1793@gmail.com>
2023-06-12 15:09:07 +0900

    0612

--- Lambda/LambdaTest2.java
+++ Lambda/LambdaTest2.java
@@ -5,21 +5,21 @@ import java.util.stream.*;
 public class LambdaTest2{
 
 	public int number = 1;
-	
+
 	MyInter1 mi1 = () -> {
 		return number;
 	};
 
 
 	public static void main(String[] args) {
-		
+
 		LambdaTest2 lt2 = new LambdaTest2();
-		
+
 		System.out.println(lt2.mi1.number());
-			
-		//결과값 
+
+		//결과값.
 		//1
 	}
-	
+
 
 }
